00504bffc7eb497feb00c8c26c770762faf7fc26
embchnk <baniak996@gmail.com>
2018-03-10 18:01:49 +0100

    Add basic buttons handlers (FX) Add Model class and its methods to execute operations on database

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,49 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+import javax.swing.*;
+
 public class Controller {
+    public TextField loginField;
+    public TextField passField;
+    public Button skipButton;
+    public Button loginButton;
+    public Button registerButton;
+    public Button goToLoginButton;
+    public Button startButton;
+
+    public void handleSkipButtonClicked(ActionEvent e) {
+        System.out.println("Skip!");
+    }
+
+    public void handleLoginButtonClicked() {
+        try {
+            System.out.println("Login!");
+            System.out.println(loginField.getText());
+            System.out.println(passField.getText());
+            // admin menu:
+            // Main.getInstance().changeScene("start.fxml");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void handleRegisterButtonClicked() {
+        System.out.println("Register!");
+    }
+
+    public void handleGoToLoginButtonClicked() {
+        try {
+            Main.getInstance().changeScene("login.fxml");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void handleStartButtonClicked() {
+        System.out.println("Start without logging in");
+    }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -6,16 +6,54 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class Main extends Application {
+import java.util.Dictionary;
+import java.util.Hashtable;
 
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
+public class Main extends Application {
+    private Stage stage;
+    private static Main instance;
+
+    public Main() {
+        instance = this;
     }
 
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        stage = primaryStage;
+        this.changeScene("start.fxml");
+
+        Hashtable<String, String> dict = new Hashtable<>();
+        Model[] admins;
+        dict.put("login", "admin");
+        dict.put("password", "admin");
+        admins = (new Admin()).select(dict);
+        for (Model m : admins) {
+            System.out.println(((Admin)m).login);
+        }
+        dict.clear();
+        dict.put("model", "Boeing 272");
+        admins = (new Plane()).select(dict);
+        for (Model m : admins) {
+            System.out.println(m);
+        }
+    }
+
+    public void changeScene(String fxml) throws Exception {
+        Parent page = FXMLLoader.load(getClass().getResource(fxml));
+        Scene scene = stage.getScene();
+        if (scene == null) {
+            scene = new Scene(page);
+            stage.setScene(scene);
+        } else {
+            stage.getScene().setRoot(page);
+        }
+        stage.sizeToScene();
+        stage.show();
+    }
+
+    public static Main getInstance() {
+        return instance;
+    }
 
     public static void main(String[] args) {
         launch(args);
